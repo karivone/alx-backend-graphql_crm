6e9ed14b4746b8891b1763f0ab94815d17c6e982
karivone <karimiyvonne19@gmail.com>
2025-07-13 13:06:06 +0300

    Fix for Understanding GraphQL tasks

--- alx_backend_graphql_crm/schema.py
+++ alx_backend_graphql_crm/schema.py
@@ -2,12 +2,6 @@
 import graphene
 from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(graphene.ObjectType):
-    hello = graphene.String()
-
-    def resolve_hello(root, info):
-        return "Hello, GraphQL!"
-
 class Query(CRMQuery, graphene.ObjectType):
     pass
 
